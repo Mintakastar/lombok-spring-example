d08ae3d16cb7afe6076b3b3338bbc678a054b3f6
raffenio <mintaka.star@gmail.com>
2025-05-06 00:42:47 -0700

    Hello world!

--- src/main/java/com/raffenio/LombokSpringExampleApplication.java
+++ src/main/java/com/raffenio/LombokSpringExampleApplication.java
@@ -8,6 +8,7 @@ public class LombokSpringExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LombokSpringExampleApplication.class, args);
+		System.out.println("Hello World!");
 	}
 
 }
