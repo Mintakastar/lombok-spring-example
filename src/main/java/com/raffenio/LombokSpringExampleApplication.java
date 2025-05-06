3faaf83ea74d426a1507304feffdf2fefd1feb64
raffenio <mintaka.star@gmail.com>
2025-05-06 00:48:52 -0700

    val usage of lombok

--- src/main/java/com/raffenio/LombokSpringExampleApplication.java
+++ src/main/java/com/raffenio/LombokSpringExampleApplication.java
@@ -1,14 +1,30 @@
 	package com.raffenio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.val;
 
 @SpringBootApplication
 public class LombokSpringExampleApplication {
 
+	private static final Logger log= LoggerFactory.getLogger(LombokSpringExampleApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LombokSpringExampleApplication.class, args);
-		System.out.println("Hello World!");
+		
+		final String username1="@raffenio"; 
+		log.info("username : {} ",username1);
+		
+		//username1="@raffenio"; this fails as this is a final string
+		
+		val username="@raffenio";  //lombok constant
+		log.info("username : {} ",username);
+		//username=""; this fails as this is a lombok variable
+		
+		
 	}
 
 }
